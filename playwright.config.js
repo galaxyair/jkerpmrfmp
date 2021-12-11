772f837f20b3cf7f42e8dca8e2ab1716d31de26b
unknown <permiakov2001@gmail.com>
2021-12-11 19:36:39 +0200

    F

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+    use: {
+      headless: true,
+      channel: "chrome"
+    },
+  };
+  
+  module.exports = config;
